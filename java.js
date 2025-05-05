642a872ea1e8292f7df946181af42a5ae44e5276
JuanDiegoM16 <jmolinabu@unal.edu.co>
2025-05-05 12:49:11 -0500

    Actualizacion de todo el codigo

--- java.js
+++ java.js
@@ -1,3 +1,21 @@
 function init(){
-    
+    var card = document.getElementById("card1")
+    card = card.innerHTML=
+    `
+    <div>
+        <h1> Esta es la tarjeta del primer producto </h1>
+        <h2 id="ProductoB">Producto B:</h2>
+        <input type="number" id="cantidad">
+        <button onclick="agregarcarrito()" type="button" class="btn btn-success boton">Añadir</button>
+    </div>
+    `
+}
+function agregarcarrito(){
+    var cantidad = document.getElementById("cantidad").value
+    var nombre = document.getElementById("ProductoB")
+    var carrito = document.getElementById("carrito")
+    carrito = carrito.innerHTML=
+    `
+    <h1>Producto: ${nombre} - Cantidad: ${cantidad}</h1>
+    `
 }
